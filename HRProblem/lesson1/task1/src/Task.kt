148a4a6d2d746d1da0aa341e48376a6b6d40c46e
ptyagicodecamp <ptyagicodecamp@gmail.com>
2019-05-16 15:13:13 -0700

    added HR problem

--- HRProblem/lesson1/task1/src/Task.kt
+++ HRProblem/lesson1/task1/src/Task.kt
@@ -1,1 +1,109 @@
-// type your solution here
+import com.sun.org.apache.xpath.internal.operations.Bool
+
+/**
+ * Classic Hospitals-Residents assignment problem
+ */
+object HRProblem{
+
+    /**
+     * Residents make proposal to be matched against Hospitals of their choice
+     */
+    fun matchResidentsToHospitals(listOfResidents: ArrayList<Resident>,
+                                  listOfHospitals: ArrayList<Hospital>): ArrayList<Hospital> {
+
+        var residents = listOfResidents
+        var hospitals = listOfHospitals
+
+        while (residents.isNotEmpty()) {
+            var resident = residents.removeAt(0)
+
+            while (resident.hospitalsPreferred.isNotEmpty()) {
+                var potentialHospital = resident.hospitalsPreferred.removeAt(0)
+
+                if (potentialHospital.considerResident(resident)) {
+                    var removedResident = potentialHospital.acceptResident(resident)
+                    if (removedResident != null) {
+                        //unmatched resident goes back in pool to be re-considered again fo next best match
+                        residents.add(0, removedResident)
+                    }
+                    resident.hospitalsPreferred = resident.hospitalsPreferred
+                }
+
+            }
+
+        }
+
+        return hospitals
+    }
+
+
+}
+
+class Resident(residentName: String) {
+    var name = residentName
+    var hospitalsPreferred = ArrayList<Hospital>()
+}
+
+class Hospital(hospitalName: String, limit: Int) {
+    var name = hospitalName
+    var limit = limit
+    var residentsPreferred = ArrayList<Resident>()
+    var residentsAccepted = ArrayList<Resident>()
+
+    //consider accepting resident
+    fun considerResident(resident: Resident): Boolean {
+        if (residentsPreferred.contains(resident)) {
+            if(!limitReached()) {
+                return true
+            } else {
+                //check if this resident is better than last accepted resident
+                if (isPreferredResident(resident, lastAcceptedResident())) {
+                    return true
+                }
+            }
+        }
+
+        return false
+    }
+
+    //accept given resident and return removedResident (less appropriate match)
+    fun acceptResident(resident: Resident): Resident? {
+        var removedResident: Resident? = null
+        if (limitReached()) {
+            removedResident = lastAcceptedResidentRemoved()
+        }
+
+        if (residentsAccepted.isEmpty() ||
+                isPreferredResident(lastAcceptedResident(), resident)) {
+            residentsAccepted.add(resident)
+        } else {
+            for (i in 0 until residentsAccepted.size) {
+                if (isPreferredResident(resident, residentsAccepted.get(i))) {
+                    residentsAccepted.add(i, resident)
+                    break
+                }
+            }
+        }
+
+        return removedResident
+    }
+
+    fun limitReached() : Boolean {
+        return residentsAccepted.size >= this.limit
+    }
+
+    //Resident1 is higher in preference list than Resident2
+    fun isPreferredResident(resident1: Resident, resident2: Resident): Boolean {
+        return residentsPreferred.indexOf(resident1) < residentsPreferred.indexOf(resident2)
+    }
+
+    fun lastAcceptedResident(): Resident {
+        return residentsAccepted.get(residentsAccepted.size - 1)
+    }
+
+    fun lastAcceptedResidentRemoved(): Resident {
+        return residentsAccepted.removeAt(residentsAccepted.size - 1)
+    }
+
+
+}
--- HRProblem/lesson1/task1/test/Tests.kt
+++ HRProblem/lesson1/task1/test/Tests.kt
@@ -1,9 +1,35 @@
 import org.junit.Assert
 import org.junit.Test
+import Hospital
+import Resident
+import HRProblem
 
 class Test {
-    @Test fun testSolution() {
-        //TODO: implement your test here
-        Assert.assertTrue("Tests not implemented for the task", false)
+    @Test fun testHRAssignment() {
+
+        var h1 = Hospital("H1", 2)
+        var h2 = Hospital("H2", 2)
+        var h3 = Hospital("H3", 2)
+
+        var listOfHospitals = arrayListOf<Hospital>(
+                h1, h2, h3
+        )
+
+
+        var listOfResidents = arrayListOf<Resident>(
+                Resident("R1").apply {
+                    hospitalsPreferred = arrayListOf(h1, h3)},
+                Resident("R2").apply {
+                    hospitalsPreferred = arrayListOf(h2, h3)},
+                Resident("R3").apply {
+                    hospitalsPreferred = arrayListOf(h1, h2)}
+        )
+
+
+
+        var hrProblem = HRProblem.matchResidentsToHospitals(listOfResidents, listOfHospitals)
+
+        for (i in 0 until hrProblem.size)
+            println(hrProblem.get(i).name)
     }
 }
